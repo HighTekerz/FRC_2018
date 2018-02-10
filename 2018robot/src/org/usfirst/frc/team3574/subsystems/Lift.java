1bff5e41d17d9068147bd2819ec36071f38d16a6
FRC2018 <emma.shusterman@gmail.com>
2018-02-09 20:32:51 -0800

    added Lift subsystem and skeleton inside

--- 2018robot/src/org/usfirst/frc/team3574/subsystems/Lift.java
+++ 2018robot/src/org/usfirst/frc/team3574/subsystems/Lift.java
@@ -1,5 +1,11 @@
 package org.usfirst.frc.team3574.subsystems;
 
+import java.lang.invoke.SwitchPoint;
+
+import org.usfirst.frc.team3574.robot.RobotMap;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -7,12 +13,71 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Lift extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	TalonSRX liftSim = new TalonSRX(RobotMap.LiftMotor); 
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
+
+	public enum LifterHeights {
+		//not the actual values yet
+		SCALE_LOW (3574), SCALE_MED (5743), SCALE_HIGH (7435), SWITCH_LOW (4357), SWITCH_HIGH (5347);
+		private int numVal;
+
+		LifterHeights(int numVal) {
+			this.numVal = numVal;
+		}
+
+		public int getNumVal() {
+			return numVal;
+		}
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+	public void initDefaultCommand() {
+
+	}
+
+	public int getEncoderValue() {
+
+		return liftSim.getSensorCollection().getPulseWidthPosition();
+	}
+
+	public void setToSETMotorValues(LifterHeights setValues) {
+		switch (setValues) {
+		case SCALE_LOW:
+
+			break;
+
+		case SCALE_MED:
+
+			break;
+			
+		case SCALE_HIGH:
+
+			break;
+
+		case SWITCH_LOW:
+
+			break;
+
+		case SWITCH_HIGH:
+			
+			break;
+
+
+		default:
+			break;
+
+		}
+	}
+
 }
 
